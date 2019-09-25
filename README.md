6e19fc45faa2e9d396784d8ea3455b595d892ee2
Jeffrey Mann <jwmann22@gmail.com>
2019-09-24 21:24:11 -0400

    got ajax logging working response from NYT

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # NYT-Search
-
-api-key: 972e655f-4d6b-4c97-bf67-668b9e2655fb
--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+    let apiKey = "6QmRZUno9AZT5oE7pYw89j24QEXhGEhO";
+    let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + apiKey;
 
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response){
+        console.log(response);
+    });
 });
+
